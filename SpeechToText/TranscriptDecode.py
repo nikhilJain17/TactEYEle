d512f9c6842cfd608b7860da78a3eb5b237dead8
Stephanie A <almeidasmail@gmail.com>
2017-01-22 01:23:03 -0500

    Update TranscriptDecode.py

--- SpeechToText/TranscriptDecode.py
+++ SpeechToText/TranscriptDecode.py
@@ -1,8 +1,8 @@
 from TestWatson import recordAndTranscribeAudio
 import SendKeys
 from time import sleep
-
-Set WshShell = WScript.CreateObject("WScript.Shell")
+import subprocess
+import pyautogui
 
 
 def determineAndWriteTranscript():
@@ -17,61 +17,69 @@ def determineAndWriteTranscript():
 			# text
 			if '"transcript": ' in text:
 				transcript = text[30:(len(text) - 3)]
-				newTranscript = ''
-				
-				for c in transcript:
-					if (c == ' '):
-						newTranscript += "{SPACE}"
-					else:
-						newTranscript += c
-				# insert code for calling where you want to write the code
-				sleep(3)
-				SendKeys.SendKeys(newTranscript)
-				break
+				transcriptSansTranscribe = ''
+				if (determineIfWrite(transcript)):	
+					transcriptSansTranscribe = transcript[11:(len(transcript))]
+					SendKeys.SendKeys(transcriptSansTranscribe, with_spaces = True)
+					break
+				else:
+					executeFunctions(transcript)
 			elif f.readline() == '':
 				break
                 
-def executeFunctions():
+def determineIfWrite(newTranscript):
+	if "transcribe" in newTranscript:
+		return True
+	else:
+		return False
+
+def executeFunctions(newTranscript):
     
-    if newTranscript == "Open Explorer":
-        subprocess.call(["C:\\Windows\\explorer.exe"])
-    
-    elif newTranscript == "Open Chrome":
+    if newTranscript == "open chrome":
         subprocess.call(["C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"])
         
-    elif newTranscript == "Open Notepad":
-        subprocess.call(["C:\\WINDOWS\\system32\\notepad.exe"])
+    # elif newTranscript == "open word":
+    #     subprocess.call(["C:\\Program Files\\Microsoft Office\\Office14\\WINWORD.exe"])
         
-    elif newTranscript == "Alt Tab":
-        SendKeys(%{TAB})
+    elif newTranscript == "alt tab":
+        pyautogui.hotkey('alt','tab')
         
-    elif newTranscript == "Control Tab":
-        SendKeys(^{TAB})
+    elif newTranscript == "control tab":
+        pyautogui.hotkey('ctrl','tab')
         
-    elif newTranscript == "Control Delete":
-        SendKeys(^{DEL})
+    elif newTranscript == "control delete":
+        pyautogui.hotkey('ctrl','delete')
         
-    elif newTranscript == "Windows Search":
-        WshShell.SendKeys "({LWIN}(s))"
-        
-    elif newTranscript == "Chrome Search":
-        SendKeys(^(l))
+    elif newTranscript == "select all":
+    	pyautogui.hotkey('ctrl', 'a')
 
-    elif newTranscript == "Control Find":
-        SendKeys(^(f))
-    
-    elif newTranscript == "Chrome Tab":
-        SendKeys(^(t))
+    elif newTranscript == "windows search":
+        pyautogui.hotkey('win','s')
         
-    elif newTranscript == "Chrome Window":
-        SendKeys(^(n))
+    #must open chrome first to use these
+    elif newTranscript == "chrome search":
+        pyautogui.hotkey('ctrl','l')
+
+    elif newTranscript == "control find":
+        pyautogui.hotkey('ctrl','f')
     
-    elif newTranscript == "Close Tab":
-        SendKeys(^(w))
-        
+    elif newTranscript == "chrome tab":
+        pyautogui.hotkey('ctrl','t')
+    
+    elif newTranscript == "close tab":
+        pyautogui.hotkey('ctrl','w')
+
+    elif newTranscript == "page down":
+    	pyautogui.press('pgdn')
+    
+    elif newTranscript == "page up":
+    	pyautogui.press('pgup')
+    
+    elif newTranscript == "enter":
+    	pyautogui.press('enter')
+
+    elif newTranscript == "tab":
+    	pyautogui.press('tab')
+    
     else:
-        print "DHRUV"
-
-            
-determineAndWriteTranscript()
-determineAndWriteTranscript()
+        print "Error: Try Again"
