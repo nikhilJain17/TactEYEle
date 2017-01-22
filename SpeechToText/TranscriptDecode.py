ad9fcec5492b7f278c2638950a70bc7e606759a3
Ali Mohamad <alimohamad1122@gmail.com>
2017-01-21 22:18:44 -0500

    Add Fuctionality
    Added:
    Alt-Tab
    Ctrl-T/W/N/F/L/Del/Tab
    File Execution

--- SpeechToText/TranscriptDecode.py
+++ SpeechToText/TranscriptDecode.py
@@ -2,6 +2,9 @@ from TestWatson import recordAndTranscribeAudio
 import SendKeys
 from time import sleep
 
+Set WshShell = WScript.CreateObject("WScript.Shell")
+
+
 def determineAndWriteTranscript():
 
 	while True:
@@ -27,3 +30,48 @@ def determineAndWriteTranscript():
 				break
 			elif f.readline() == '':
 				break
+                
+def executeFunctions():
+    
+    if newTranscript == "Open Explorer":
+        subprocess.call(["C:\\Windows\\explorer.exe"])
+    
+    elif newTranscript == "Open Chrome":
+        subprocess.call(["C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"])
+        
+    elif newTranscript == "Open Notepad":
+        subprocess.call(["C:\\WINDOWS\\system32\\notepad.exe"])
+        
+    elif newTranscript == "Alt Tab":
+        SendKeys(%{TAB})
+        
+    elif newTranscript == "Control Tab":
+        SendKeys(^{TAB})
+        
+    elif newTranscript == "Control Delete":
+        SendKeys(^{DEL})
+        
+    elif newTranscript == "Windows Search":
+        WshShell.SendKeys "({LWIN}(s))"
+        
+    elif newTranscript == "Chrome Search":
+        SendKeys(^(l))
+
+    elif newTranscript == "Control Find":
+        SendKeys(^(f))
+    
+    elif newTranscript == "Chrome Tab":
+        SendKeys(^(t))
+        
+    elif newTranscript == "Chrome Window":
+        SendKeys(^(n))
+    
+    elif newTranscript == "Close Tab":
+        SendKeys(^(w))
+        
+    else:
+        print "DHRUV"
+
+            
+determineAndWriteTranscript()
+determineAndWriteTranscript()
